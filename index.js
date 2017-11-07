f095d8c926aca41a7d5b0afbf359a7c88cc13d2a
Bryan Ortiz <bryan@bryanortiz.com>
2017-11-07 16:36:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ function updateDriverWithKeyAndValue (driver, key, value) {
   return Object.assign({}, driver, { [key]: value });
 }
 
-function destructivelyUpdateDriverWithKeyAndValue(driver, key, value)
+function destructivelyUpdateDriverWithKeyAndValue(driver, key, value){
   driver[key] = value;
   return driver;
+}
+  function deleteFromDriverByKey()
